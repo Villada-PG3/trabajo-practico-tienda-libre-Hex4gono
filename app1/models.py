0eff406dc27b3a2c8fa9c24eb3ff7d9ac1335b8d
Ezequiel <andradagezequiel@gmail.com>
2026-08-03 11:30:19 -0300

    commit clase

--- app1/models.py
+++ app1/models.py
@@ -1,17 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-    descripcion = models.TextField()
+    nombre = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ['nombre']
+
     def __str__(self):
         return self.nombre
 class Producto(models.Model):
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name='productos',
+        null = True,
+        blank = True
+    )
     nombre = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    marca = models.CharField(max_length=50, default='Marca Desconocida')
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        ordering = ['nombre']
+
     def __str__(self):
-        return self.nombre
+        return f'{self.nombre} - {self.marca} - ${self.precio} - Stock: {self.stock}'
