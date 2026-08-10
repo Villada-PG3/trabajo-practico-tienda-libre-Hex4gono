4c6671f0d8a42aa0e653a27bb146ed3f76701d72
Ezequiel <andradagezequiel@gmail.com>
2026-08-10 08:47:24 -0300

    feat: primera vista y template para home y acerca-de-mi.

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello_world(request):
-    return render(request, "hello_world.html")
+    return render(request, "app1/hello_world.html")
+
+def home(request):
+    return render(request, "app1/home.html")
+
+def acerca_de_mi(request):
+    return render(request, "app1/acerca_de_mi.html")
